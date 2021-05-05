f1128a802943a749622178d481c3ba616cbecba9
Solyankin Ilia <evil.enot.00@gmail.com>
2021-05-05 12:58:05 +0300

    Done friends, add friends and callback pages

--- app/src/main/java/com/example/flashcards/fragments/FriendsPageFragment.kt
+++ app/src/main/java/com/example/flashcards/fragments/FriendsPageFragment.kt
@@ -9,20 +9,21 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.flashcards.R
 
-class UserGroupsPageFragment: Fragment() {
+class FriendsPageFragment: Fragment() {
 
-    private lateinit var addNewGroupButton: ImageButton
+    private lateinit var addNewFriend: ImageButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View  = inflater.inflate(R.layout.user_groups_page, container, false)
+    ): View = inflater.inflate(R.layout.friends_page, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        addNewGroupButton=view.findViewById(R.id.add_friend)
-        addNewGroupButton.setOnClickListener {
-            findNavController().navigate(R.id.action_to_user_groups_to_addNewGroupPageFragment)
+        addNewFriend = view.findViewById(R.id.add_friend)
+        addNewFriend.setOnClickListener {
+            findNavController().navigate(R.id.action_to_friends_to_addFriendPageFragment)
         }
     }
+
 }
