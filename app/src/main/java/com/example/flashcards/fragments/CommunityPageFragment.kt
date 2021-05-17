86983fe1bd490a2158d45a80a0b395eb639bf490
Solyankin Ilia <evil.enot.00@gmail.com>
2021-05-17 09:35:35 +0300

    Add callback buttons, changed README.md and menu

--- app/src/main/java/com/example/flashcards/fragments/CommunityPageFragment.kt
+++ app/src/main/java/com/example/flashcards/fragments/CommunityPageFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.flashcards.R
 
 class CommunityPageFragment: Fragment() {
+
+    private lateinit var callbackButton: ImageButton
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,6 +20,9 @@ class CommunityPageFragment: Fragment() {
     ): View = inflater.inflate(R.layout.community_page, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        callbackButton = view.findViewById(R.id.callback_button)
+        callbackButton.setOnClickListener {
+            findNavController().navigate(R.id.action_communityPageFragment_to_to_callback)
+        }
     }
 }
--- app/src/main/java/com/example/flashcards/fragments/GroupPageFragment.kt
+++ app/src/main/java/com/example/flashcards/fragments/GroupPageFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.flashcards.R
 
 class GroupPageFragment: Fragment() {
+
+    private lateinit var callbackButton: ImageButton
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,6 +23,9 @@ class GroupPageFragment: Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        callbackButton = view.findViewById(R.id.callback_button)
+        callbackButton.setOnClickListener {
+            findNavController().navigate(R.id.action_groupPageFragment_to_to_callback)
+        }
     }
 }
