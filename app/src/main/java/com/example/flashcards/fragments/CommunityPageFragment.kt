50b8229496df4bf8a04f6cbc2def260f1dc781c4
Solyankin Ilia <evil.enot.00@gmail.com>
2021-05-17 14:07:12 +0300

    Changed community page

--- app/src/main/java/com/example/flashcards/fragments/CommunityPageFragment.kt
+++ app/src/main/java/com/example/flashcards/fragments/CommunityPageFragment.kt
@@ -10,9 +10,6 @@ import androidx.navigation.fragment.findNavController
 import com.example.flashcards.R
 
 class CommunityPageFragment: Fragment() {
-
-    private lateinit var callbackButton: ImageButton
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,9 +17,6 @@ class CommunityPageFragment: Fragment() {
     ): View = inflater.inflate(R.layout.community_page, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        callbackButton = view.findViewById(R.id.callback_button)
-        callbackButton.setOnClickListener {
-            findNavController().navigate(R.id.action_communityPageFragment_to_to_callback)
-        }
+        super.onViewCreated(view, savedInstanceState)
     }
 }
